647b4d1a0fdd3a8a221e310d88f7f03eb653dac0
vishnuvardhanjs-alt <vichudsvv@gmail.com>
2024-08-28 09:42:26 +0530

    init

--- src/App.js
+++ src/App.js
@@ -1,23 +1,96 @@
 import logo from './logo.svg';
 import './App.css';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faChessRook, faChessBishop, faChessQueen, faChessPawn,faChessKnight, faChessKing } from '@fortawesome/free-solid-svg-icons';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+  return (    
+    <div className="main-cont">
+      <div className="chessboard">
+        <table>
+          <tr>
+            <td className='white'><FontAwesomeIcon className='pieces' icon={faChessRook} /></td>
+            <td className='green'><FontAwesomeIcon className='pieces' icon={faChessKnight} /></td>
+            <td className='white'><FontAwesomeIcon className='pieces' icon={faChessBishop} /></td>
+            <td className='green'><FontAwesomeIcon className='pieces' icon={faChessKing} /></td>
+            <td className='white'><FontAwesomeIcon className='pieces' icon={faChessQueen} /></td>
+            <td className='green'><FontAwesomeIcon className='pieces' icon={faChessBishop} /></td>
+            <td className='white'><FontAwesomeIcon className='pieces' icon={faChessKnight} /></td>
+            <td className='green'><FontAwesomeIcon className='pieces' icon={faChessRook} /></td>
+          </tr>
+          <tr>
+            <td className='green'><FontAwesomeIcon className='pieces' icon={faChessPawn} /></td>
+            <td className='white'><FontAwesomeIcon className='pieces' icon={faChessPawn} /></td>
+            <td className='green'><FontAwesomeIcon className='pieces' icon={faChessPawn} /></td>
+            <td className='white'><FontAwesomeIcon className='pieces' icon={faChessPawn} /></td>
+            <td className='green'><FontAwesomeIcon className='pieces' icon={faChessPawn} /></td>
+            <td className='white'><FontAwesomeIcon className='pieces' icon={faChessPawn} /></td>
+            <td className='green'><FontAwesomeIcon className='pieces' icon={faChessPawn} /></td>
+            <td className='white'><FontAwesomeIcon className='pieces' icon={faChessPawn} /></td>
+          </tr>
+          <tr>
+            <td className='white'></td>
+            <td className='green'></td>
+            <td className='white'></td>
+            <td className='green'></td>
+            <td className='white'></td>
+            <td className='green'></td>
+            <td className='white'></td>
+            <td className='green'></td>
+          </tr>
+          <tr>
+            <td className='green'></td>
+            <td className='white'></td>
+            <td className='green'></td>
+            <td className='white'></td>
+            <td className='green'></td>
+            <td className='white'></td>
+            <td className='green'></td>
+            <td className='white'></td>
+          </tr>
+          <tr>
+            <td className='white'></td>
+            <td className='green'></td>
+            <td className='white'></td>
+            <td className='green'></td>
+            <td className='white'></td>
+            <td className='green'></td>
+            <td className='white'></td>
+            <td className='green'></td>
+          </tr>
+          <tr>
+            <td className='green'></td>
+            <td className='white'></td>
+            <td className='green'></td>
+            <td className='white'></td>
+            <td className='green'></td>
+            <td className='white'></td>
+            <td className='green'></td>
+            <td className='white'></td>
+          </tr>
+          <tr>
+            <td className='white'><FontAwesomeIcon className='pieces whp' icon={faChessPawn} /></td>
+            <td className='green'><FontAwesomeIcon className='pieces whp' icon={faChessPawn} /></td>
+            <td className='white'><FontAwesomeIcon className='pieces whp' icon={faChessPawn} /></td>
+            <td className='green'><FontAwesomeIcon className='pieces whp' icon={faChessPawn} /></td>
+            <td className='white'><FontAwesomeIcon className='pieces whp' icon={faChessPawn} /></td>
+            <td className='green'><FontAwesomeIcon className='pieces whp' icon={faChessPawn} /></td>
+            <td className='white'><FontAwesomeIcon className='pieces whp' icon={faChessPawn} /></td>
+            <td className='green'><FontAwesomeIcon className='pieces whp' icon={faChessPawn} /></td>
+          </tr>
+          <tr>
+            <td className='green'><FontAwesomeIcon className='pieces whp' icon={faChessRook} /></td>
+            <td className='white'><FontAwesomeIcon className='pieces whp' icon={faChessKnight} /></td>
+            <td className='green'><FontAwesomeIcon className='pieces whp' icon={faChessBishop} /></td>
+            <td className='white'><FontAwesomeIcon className='pieces whp' icon={faChessQueen} /></td>
+            <td className='green'><FontAwesomeIcon className='pieces whp' icon={faChessKing} /></td>
+            <td className='white'><FontAwesomeIcon className='pieces whp' icon={faChessBishop} /></td>
+            <td className='green'><FontAwesomeIcon className='pieces whp' icon={faChessKnight} /></td>
+            <td className='white'><FontAwesomeIcon className='pieces whp' icon={faChessRook} /></td>
+          </tr>
+        </table>
+      </div>
     </div>
   );
 }
